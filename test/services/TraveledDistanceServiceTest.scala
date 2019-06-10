0827bec202ca9a57a1cac34e1ca369c0032e5373
anebril <anebril@seznam.cz>
2019-06-10 18:25:17 +0200

    Fixed failing test

--- test/services/TraveledDistanceServiceTest.scala
+++ test/services/TraveledDistanceServiceTest.scala
@@ -40,20 +40,20 @@ class TraveledDistanceServiceTest
 
       val service = app.injector.instanceOf[TraveledDistanceService]
 
-      await(service.findMinKilometers("Praha", "Brno")) mustBe DistanceResponse(true, Some(280 + 80))
-      await(service.findMinKilometers("Brno", "Praha")) mustBe DistanceResponse(true, Some(200))
-      await(service.findMinKilometers("Praha", "Praha")) mustBe DistanceResponse(true, Some(64))
+      await(service.findMinKilometers("praha", "brno")) mustBe DistanceResponse(true, Some(280 + 80))
+      await(service.findMinKilometers("brno", "praha")) mustBe DistanceResponse(true, Some(200))
+      await(service.findMinKilometers("praha", "praha")) mustBe DistanceResponse(true, Some(64))
 
-      await(service.findMinKilometers("Brno", "Beroun")) mustBe DistanceResponse(false, None)
-      await(service.findMinKilometers("Praha", "Kolin")) mustBe DistanceResponse(false, None)
-      await(service.findMinKilometers("Kolin", "Praha")) mustBe DistanceResponse(false, None)
-      await(service.findMinKilometers("Kolin", "Nymburk")) mustBe DistanceResponse(false, None)
+      await(service.findMinKilometers("brno", "beroun")) mustBe DistanceResponse(false, None)
+      await(service.findMinKilometers("praha", "kolin")) mustBe DistanceResponse(false, None)
+      await(service.findMinKilometers("kolin", "praha")) mustBe DistanceResponse(false, None)
+      await(service.findMinKilometers("kolin", "nymburk")) mustBe DistanceResponse(false, None)
 
-      await(service.findMinDuration("Praha", "Brno")) mustBe DurationResponse(true, Some(2))
-      await(service.findMinDuration("Brno", "Praha")) mustBe DurationResponse(true, Some(1))
-      await(service.findMinDuration("Praha", "Praha")) mustBe DurationResponse(true, Some(0))
+      await(service.findMinDuration("praha", "brno")) mustBe DurationResponse(true, Some(2))
+      await(service.findMinDuration("brno", "praha")) mustBe DurationResponse(true, Some(1))
+      await(service.findMinDuration("praha", "praha")) mustBe DurationResponse(true, Some(0))
 
-      await(service.findMinDuration("Kolin", "Nymburk")) mustBe DurationResponse(false, None)
+      await(service.findMinDuration("kolin", "nymburk")) mustBe DurationResponse(false, None)
 
       an[InvalidFromToException] must be thrownBy
         await(service.findMinDuration(" ", ""))
